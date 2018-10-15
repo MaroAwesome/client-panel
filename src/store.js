5f8436b035c29d070a51a0ed17003985046fc158
maroawesome <maroawesome@gmail.com>
2018-10-15 17:01:48 +0300

    fixed react dev tool debug

--- src/store.js
+++ src/store.js
@@ -47,9 +47,6 @@ const initialState = {};
 const store = createStoreWithFirebase(
   rootReducer,
   initialState,
-  compose(
-    reactReduxFirebase(firebase),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  compose(reactReduxFirebase(firebase))
 );
 export default store;
